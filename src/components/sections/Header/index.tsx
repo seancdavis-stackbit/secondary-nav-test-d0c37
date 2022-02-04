8c3d6c0642e52225b874c1984d8e54a229fc261f
Stackbit Code Editor <projects@stackbit.com>
2022-02-04 11:49:45 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -171,7 +171,7 @@ function MobileMenu(props) {
                     {(primaryLinks.length > 0 || socialLinks.length > 0) && (
                         <div className="flex flex-col justify-center flex-grow px-4 py-20 space-y-12">
                             {primaryLinks.length > 0 && (
-                                {primaryLinks.map(link => 
+                                {primaryLinks.map(list => 
                                     <HeaderLinkList heading={link.heading} links={link.links} />
                                     )}
                             )}
